04410b54d0bac92084da795191f4ecdf1563db8c
Kevin Kwok <antimatter15@gmail.com>
2016-09-11 01:54:12 -0400

    first codes

---
+++ default.js
@@ -0,0 +1,42 @@
+import socketIO from 'socket.io-client'
+import { reimportModule } from './build.js'
+
+const backendUrl = '' // same URL
+let socket = socketIO(backendUrl)
+
+socket.on('connect', () => {
+    console.log('hot reload connected to watcher on ', backendUrl)
+    socket.emit('identification', navigator.userAgent)
+    socket.emit('package.json', function (pjson) {
+        // console.log('stuff', pjson)
+        // self.pjson = pjson // maybe needed in the future?
+        // self.jspmConfigFile = pjson.jspm.configFile || 'config.js'
+    })
+})
+
+socket.on('reload', () => {
+    console.log('whole page reload requested')
+})
+
+socket.on('change', (e) => {
+    var path = e.path,
+        norm = System.normalizeSync(path)
+
+    if(System.bundles[path]){
+        console.log('BUILT BUNDLE', path)
+    }else if(System.has(norm)){
+        console.log('UPDATE MODULE', path)
+
+        reimportModule(norm)
+    }else{
+        console.log('NOT FOUND', path)
+    }
+})
+
+window.onerror = (err) => {
+    socket.emit('error', err)  // emitting errors for jspm-dev-buddy
+}
+
+socket.on('disconnect', () => {
+    console.log('hot reload disconnected from ', backendUrl)
+})
---
+++ reload.js
@@ -0,0 +1,153 @@
+// Carbide's hot-reloading engine is a bit different from
+// the hot module replacement packages that you find in
+// Webpack, Browserify, or SystemJS. 
+
+// We don't want to unload/reload any modules which don't
+// explicitly declare themselves to be safe to reload. 
+// but we also want to allow hot reloading to work for
+// as many modules as possible out of the box. 
+
+// At the same time, we want module reloading to happen
+// as fast as possible. We want to avoid recompiling
+// files that don't need to be recompiled, and we want
+// to avoid executing files which don't need to be executed. 
+
+// Key to accomplishing this is the fact that if you have
+// code like this:
+
+// module b:
+// import a from 'module-a'
+// export default function(){
+// 	return a + 4
+// }
+
+// you don't actually need to re-evaluate 'module-b'
+// when 'module-a' changes. Instead, we call some internal
+// setters to replace the reference of 'module-a' in the 
+// live version of 'module-b'. 
+
+// and when you do have to re-evaluate a module,
+// most of the time you don't actually have to transpile
+// the thing. 
+
+// TODO: figure out how to get this to work for mutually recursive modules
+
+
+export async function reimportModule(path){
+	console.log('reimporting', path)
+	var old_module = await unloadedModule(path)
+
+	// this is the real meaty bit
+	System.delete(path)
+	await System.import(path)
+
+	await reloadedModule(path, old_module)
+	bubbleModule(path)
+
+	return System.get(path);
+}
+
+export async function refreshModule(path){
+	console.log('refreshing', path)
+	var old_module = cloneModule(await unloadedModule(path))
+
+	var record = System._loader.moduleRecords[path]
+	record.execute()
+
+	await reloadedModule(path, old_module)
+	bubbleModule(path)
+
+	return System.get(path);
+}
+
+
+
+async function bubbleModule(path){
+	var new_module = System.get(path)
+	var importing = []
+	for(let [imp, i] of listImporterIndices(path)){
+		// call setters
+		let rec = System._loader.moduleRecords[imp.address]
+		rec.setters[i](new_module)
+
+		// get a reference to the module
+		importing.push(imp)
+	}
+
+
+	for(let imp of importing){
+		let mod = System.get(imp.address)
+
+		if(mod.__forceReload){
+			await refreshModule(imp.address)
+		}else{
+			await bubbleRender(imp.address)
+		}
+	}
+}
+
+
+async function bubbleRender(path){
+	console.log('bubble', path)
+
+	let mod = System.get(path)
+	
+	if(mod.__render){
+		return await mod.__render()
+	}
+
+	for(let imp of listImporters(path)){
+		await bubbleRender(imp.address)
+	}
+}
+
+
+async function unloadedModule(path){
+	var mod = System.get(path)
+	if(mod && mod.__unload){
+		await mod.__unload()
+	}
+	return mod;
+}
+
+async function reloadedModule(path, old_module){
+	// don't fire reloadedModule on the first mount
+	if(old_module){
+		var mod = System.get(path)
+		if(mod.__reload){
+			await mod.__reload(old_module)
+		}
+	}
+}
+
+
+function cloneModule(mod){
+	// when re-executing modules, the actual module doesn't change
+	// so instead of passing a fully qualified module, we just clone
+	// all the exports and return that object
+
+	var clone = {}
+    Object.getOwnPropertyNames(mod)
+    	.forEach(key => clone[key] = mod[key]);
+
+    return clone
+}
+
+
+function listImporterIndices(name){
+	var imports = [];
+	Object.keys(System.loads).forEach((moduleName) => {
+		var mod = System.loads[moduleName]
+		mod.deps.forEach((dependantName, i) => {
+			if(name === mod.depMap[dependantName]){
+				imports.push([mod, i])
+			}
+		})
+	})
+	return imports;
+}
+
+
+function listImporters(path){
+	return listImporterIndices(path).map(k => k[0])
+}
